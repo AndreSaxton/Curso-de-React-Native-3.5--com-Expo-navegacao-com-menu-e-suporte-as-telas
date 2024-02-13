1d97bf97dc17efd8a1254327e9f0c2e4dfb1052c
André Saxton <andre_saxton@hotmail.com>
2024-02-13 20:18:40 -0300

    Tela de serviços

---
+++ src/telas/Servicos/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { SafeAreaView, StatusBar, Text } from 'react-native'
+
+const servicos = [
+    {
+        id: 1,
+        nome: "Banho",
+        oreci: 79.9,
+        descricao: "NÃO DE BANHO NO SEU GATO! Mas se precisar nós damos!",
+    },
+    {
+        id: 2,
+        nome: "Vacina V4",
+        preco: 89.9,
+        descricao: "Uma dose da vacina V4. Seu gato precisa de duas."
+    },
+    {
+        id: 3,
+        nome: "Vacina Antirábica",
+        preco: 99.9,
+        descricao: "Uma dose da vacina antirábica. Seu gato precisa de uma por ano.",
+    },
+]
+
+export default function Servicos() {
+    return <SafeAreaView>
+        <StatusBar />
+        <Text>Serviços!</Text>
+    </SafeAreaView>
+}
